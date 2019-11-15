1d4e435e3b40f88efc6a9bd314fdfecf4025ed27
Tarun Nanduri <tarunnanduri7@gmail.com>
2019-11-15 19:23:30 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,14 +4,12 @@ from sklearn.model_selection import train_test_split
 #read data from .csv file
 data=pd.read_csv("iris.csv")
 data.columns=["petal_length","petal_width","sepal_length","sepal_width","class"]
-#training a perceptron
 classes=data["class"]
 data=data.drop(columns="class")
 #splitting test and train data for iris
 x_train,x_test,y_train,y_test=train_test_split(data,classes)
-#training the weights for percpetron
-p=Perceptron()
-p.fit(x_train,y_train,0.5,10)
-pred=p.predict(x_test)
-print("accuracy: ",p.accuracy(pred,y_test))
-print("weights: ",p.getweights())
+model=Perceptron()
+model.fit(x_train,y_train,0.5,10)
+pred=model.predict(x_test)
+print("accuracy: ",model.accuracy(pred,y_test))
+print("weights: ",model.getweights())
